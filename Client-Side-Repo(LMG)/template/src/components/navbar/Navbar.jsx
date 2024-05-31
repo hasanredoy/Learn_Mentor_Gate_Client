15acbd900b9f7e7bf981fea46ec3d3cd00b9fe6a
unknown <redoyhasan4585@gmail.com>
2024-05-31 22:43:03 +0400

    added user photo and user icon on navbar based on user actitity

--- Client-Side-Repo(LMG)/template/src/components/navbar/Navbar.jsx
+++ Client-Side-Repo(LMG)/template/src/components/navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import{Link, NavLink}from 'react-router-dom'
 import './navbar.css'
 import logo from '../../assets/logo.png'
 import useAuth from '../../hooks/useAuth';
+import { FaSignOutAlt, FaUser } from "react-icons/fa";
+import { RxDashboard } from "react-icons/rx";
+
+
 const Navbar = () => {
   const {user}=useAuth()
   const navLinks=<div className=' flex gap-5 '>
@@ -9,6 +13,7 @@ const Navbar = () => {
     <NavLink className={'font-semibold text-lg hover:underline'} to={'/allClasses'}>All Classes</NavLink>
     <NavLink className={'font-semibold text-lg hover:underline'} to={'/teachOnLearnMonitorGate'}>Teach on Learn Monitor Gate</NavLink>
   </div>
+  
   return (
     <div className="navbar bg-base-200">
   <div className="navbar-start">
@@ -20,7 +25,7 @@ const Navbar = () => {
         {navLinks}
       </ul>
     </div>
-    <Link to={'/'} className=" text-xl font-bold"> <img className=' h-16' src={logo} alt="" /></Link>
+    <Link to={'/'} className=" text-xl font-bold"> <img className=' h-12 lg:h-16' src={logo} alt="" /></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -28,7 +33,21 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    {
+      !user?<div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className=""><div>
+        {
+          !user?.photoURL?<img src={logo} className=' w-12 lg:w-14 h-12 lg:h-14 border border-green-600 rounded-full' alt="" />:<FaUser className=' text-5xl  border border-green-600 rounded-full'></FaUser>
+        }
+      </div></div>
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 flex flex-col gap-3">
+        <li className=' text-xl font-bold'>{user?.displayName?user?.displayName:'Anonymous'}</li>
+        <Link><button className=' text-lg font-bold text-white bg-green-700 px-4 py-2 rounded-lg hover:bg-white hover:text-green-600 hover:border hover:border-green-700 w-full flex gap-2 justify-center items-center '>Dashboard <RxDashboard></RxDashboard></button></Link>
+        <button className=' text-lg font-bold text-white bg-amber-500 px-4 py-2 rounded-lg hover:bg-white hover:text-amber-500 hover:border hover:border-amber-700 flex gap-2 justify-center items-center '>Logout <FaSignOutAlt></FaSignOutAlt></button>
+      </ul>
+    </div>: <Link><button className=' text-lg font-bold text-white bg-green-700 px-4 py-2 rounded-lg hover:bg-white hover:text-green-600 hover:border hover:border-green-700 '>Login</button></Link>
+    }
+    
   </div>
 </div>
   );
