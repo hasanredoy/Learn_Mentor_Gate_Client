25ad44b88903091f99c09af6b85f3257866f9289
unknown <redoyhasan4585@gmail.com>
2024-06-02 12:59:16 +0400

    still working on popular course section and fetched courses data using tanstack

--- Client-Side-Repo(LMG)/template/src/pages/Home/OurPatners/OurPartners.jsx
+++ Client-Side-Repo(LMG)/template/src/pages/Home/OurPatners/OurPartners.jsx
@@ -1,41 +1,54 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosCommon from "../../../hooks/useAxiosCommon";
-import partnerIcon from '../../../assets/Partners/agreement.png'
+import partnerIcon from "../../../assets/Partners/agreement.png";
 import { FaChevronRight } from "react-icons/fa6";
 
 const OurPartners = () => {
-  const axiosCommon= useAxiosCommon()
-  const {data:partners=[]}=useQuery({
-    queryKey:['partners'],
-    queryFn:async()=>{
-        const res = await axiosCommon.get('/partners')
-        return res.data
-    }
-  })
- 
+  const axiosCommon = useAxiosCommon();
+  const { data: partners = [] } = useQuery({
+    queryKey: ["partners"],
+    queryFn: async () => {
+      const res = await axiosCommon.get("/partners");
+      return res.data;
+    },
+  });
+
   return (
     <div>
-      <h1 className=" text-2xl lg:text-4xl text-center font-bold">Our Supportive Network</h1>
+      <h1 className=" text-2xl lg:text-4xl text-center font-bold">
+        Our Supportive Network
+      </h1>
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10  mt-5">
-        {
-          partners?.map(partner=><div key={partner?._id} className=" p-4 rounded-md shadow-md border ">
-          <img src={partner?.logo} alt="" className="object-cover object-center mx-auto rounded-md h-44  " />
-          <div className="mt-6 mb-2">
-         
-            <h2 className="text-xl font-bold tracking-wide">{partner?.name}</h2>
+        {partners?.map((partner) => (
+          <div key={partner?._id} className=" p-4 rounded-md shadow-md border ">
+            <img
+              src={partner?.logo}
+              alt=""
+              className="object-cover object-center mx-auto rounded-md h-44  "
+            />
+            <div className="mt-6 mb-2">
+              <h2 className="text-xl font-bold tracking-wide">
+                {partner?.name}
+              </h2>
+            </div>
+            <p className="text-sm">{partner?.description}</p>
           </div>
-          <p className="text-sm">{partner?.description}</p>
-        </div>)
-        
-        }
+        ))}
         <div className=" p-4 rounded-md shadow-md border ">
-          <img src={partnerIcon} alt="" className="object-cover object-center mx-auto rounded-md h-44  " />
+          <img
+            src={partnerIcon}
+            alt=""
+            className="object-cover object-center mx-auto rounded-md h-44  "
+          />
           <div className="mt-6 mb-2">
-         
-            <h2 className="text-2xl  rounded-lg text-center font-bold ">Become Our  <span className=" text-green-600">Partner</span></h2>
-           <div className=" flex justify-center items-center mt-5">
-           <button className=" text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500"><FaChevronRight className=" text-4xl font-black"></FaChevronRight></button>
-           </div>
+            <h2 className="text-2xl  rounded-lg text-center font-bold ">
+              Become Our <span className=" text-green-600">Partner</span>
+            </h2>
+            <div className=" flex justify-center items-center mt-5">
+              <button className=" text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500">
+                <FaChevronRight className=" text-4xl font-black"></FaChevronRight>
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- Client-Side-Repo(LMG)/template/src/pages/Home/PopularCourses/PopularCourses.jsx
+++ Client-Side-Repo(LMG)/template/src/pages/Home/PopularCourses/PopularCourses.jsx
@@ -1,10 +1,12 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
 // import slide1 from '../../../assets/slide1.png'
 // import slide2 from '../../../assets/slide2.jpg'
 // import slide3 from '../../../assets/slide3.png'
@@ -14,46 +16,60 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 //   import videosIcon from '../../../assets/icons/montage.png'
 // import studentsIcon from '../../../assets/icons/group.png'
 
-
 const PopularCourses = () => {
-  return (
-    <div>
-      <h1 className=' text-2xl lg:text-4xl font-bold text-center'>Have a Look Some of Our <span className=' text-green-600 '>Popular</span> Courses</h1>
-       <div>
-       <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className='bg-black relative'>
-         
-          </div>
-          </SwiperSlide>
-        <SwiperSlide><div className=' bg-black relative'>
-       
-          </div></SwiperSlide>
-        <SwiperSlide><div className=' bg-black relative'>
-         
-          </div></SwiperSlide>
-        <SwiperSlide><div className=' bg-black relative'>
-         
-      
+  const axiosCommon = useAxiosCommon();
+  const { data: courses = [] } = useQuery({
+    queryKey: ["courses"],
+    queryFn: async () => {
+      const res = await axiosCommon.get("/courses");
+      return res.data;
+    },
+  });
 
-          </div></SwiperSlide>
-       
-        
-      </Swiper>
-       </div>
+  console.log(courses);
+  return (
+    <div className=" mt-10">
+      <h1 className=" text-2xl lg:text-4xl font-bold text-center">
+        Have a Look Some of Our{" "}
+        <span className=" text-green-600 ">Popular</span> Courses
+      </h1>
+      <div>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {courses?.map((course) => (
+            <SwiperSlide key={course?._id}>
+              <div className="card card-side bg-base-100 shadow-xl">
+                <figure>
+                  <img
+                    src={course?.Image}
+                    alt="Movie"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{course?.Title}</h2>
+                  <p>{course?.['Short description']}</p>
+                  <h2 className="card-title">Total Enrollments{course?.['Total enrollment']}</h2>
+                  <div className="card-actions justify-end">
+                    <button className="text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500">Enroll</button>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
