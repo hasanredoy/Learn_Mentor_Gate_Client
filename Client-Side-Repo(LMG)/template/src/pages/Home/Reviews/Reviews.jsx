f52521ce106f8d55471d7fa7f5a0def079c1f2a4
unknown <redoyhasan4585@gmail.com>
2024-06-02 19:57:22 +0400

    still working on  reviews section

--- Client-Side-Repo(LMG)/template/src/pages/Home/Reviews/Reviews.jsx
+++ Client-Side-Repo(LMG)/template/src/pages/Home/Reviews/Reviews.jsx
@@ -1,12 +1,13 @@
-import useAxiosCommon from '../../../hooks/useAxiosCommon';
+import useAxiosCommon from "../../../hooks/useAxiosCommon";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import {  Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 import { useQuery } from "@tanstack/react-query";
+import { FaQuoteRight } from "react-icons/fa6";
 const Reviews = () => {
   const axiosCommon = useAxiosCommon();
   const { data: reviews = [] } = useQuery({
@@ -17,45 +18,63 @@ const Reviews = () => {
     },
   });
 
-   console.log(reviews);
+  console.log(reviews);
   return (
-    <div>
-    <Swiper
-      spaceBetween={30}
-      centeredSlides={true}
-   
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation]}
-      className="mySwiper"
-    >
-      {reviews?.map((course) => (
-        <SwiperSlide className=" py-10" key={course?._id}>
-          <div className="card min-h-[400px] max-h-[400px] flex flex-col gap-5 lg:flex-row bg-base-100 shadow-xl">
-            
-            <figure className=" h-full  w-full lg:w-1/2">
-              <img
-              className=" md:h-[400px] lg:h-full w-full"
-                src={course?.Image}
-                alt="Movie"
-              />
-            </figure>
-            <div className=" w-full lg:w-1/2 flex flex-col gap-5 justify-start items-start  p-3  lg:p-10 ">
-              <h2 className=" text-3xl font-bold">{course?.Title}</h2>
-              <p className=" pr-5 lg:pr-20">{course?.['Short_description']}</p>
-              <h2 className=" text-lg font-bold">Total Enrollments: {course?.['Total_enrollment']}</h2>
-              <h2 className=" text-base font-bold">Course By: {course?.Name}</h2>
-              <div className="">
-                <button className="text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500">Enroll Now!</button>
+    <div className=" ">
+       <div>
+        <h1 className=" text-3xl text-center font-bold">Hear What People Says About Us?</h1>
+      </div>
+      <Swiper
+      
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper w-[60%] mx-auto "
+      >
+        {reviews?.map((review) => (
+          <SwiperSlide className=" py-10" key={review?._id}>
+            <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 ">
+              <div className="flex justify-between p-4">
+                <div className="flex space-x-4">
+                  <div>
+                    <img
+                      src={review?.image}
+                      alt=""
+                      className="object-cover w-12 h-12 rounded-full "
+                    />
+                  </div>
+                  <div>
+                    <h4 className="font-bold">{review?.name}</h4>
+                    <span className="text-xs ">{review?.posted_time.split('T')[0]}</span>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-2 text-yellow-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    className="w-5 h-5 fill-current"
+                  >
+                    <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+                  </svg>
+                  <span className="text-xl font-bold">{review?.rating}</span>
+                </div>
+              </div>
+              <div className="p-4 space-y-2 text-sm ">
+                <FaQuoteRight className=" text-5xl mx-auto"></FaQuoteRight>
+                <p className=" text-lg">
+               {review?.feedback_text}
+                </p>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+     
+    </div>
   );
 };
 
