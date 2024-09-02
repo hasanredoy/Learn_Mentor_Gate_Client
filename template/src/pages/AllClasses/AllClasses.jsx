3d95896be72c217d5d0600bc1ec74d54d17632ab
unknown <redoyhasan4585@gmail.com>
2024-09-02 12:59:19 +0400

    adding filter and search functionality in all classes page

--- template/src/pages/AllClasses/AllClasses.jsx
+++ template/src/pages/AllClasses/AllClasses.jsx
@@ -6,66 +6,83 @@ import useGetAllCallsesForAllClassesPageCount from "../../hooks/useGetAllCallses
 import { FaGreaterThan, FaLessThan } from "react-icons/fa6";
 import HelmetPorvider from "../../ReuseableCompo/HelmetPorvider";
 import LoadingSpinner from "../../ReuseableCompo/LoadingSpinner";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Heading from "../../ReuseableCompo/Heading";
 
 const AllClasses = () => {
-     //  get all classes length
-     const [currentPage, setCurrentPage] = useState(0);
-     const  allApprovedClassCount= useGetAllCallsesForAllClassesPageCount();
-      // console.log( allApprovedClassCount);
-     const itemsPerPage = 10;
-     const numberOfPage = Math.ceil( allApprovedClassCount / itemsPerPage);
-     //  console.log(numberOfPage);
-     let pages = [];
-     for (let num = 0; num < numberOfPage; num++) {
-       pages.push(num);
-     }
-     console.log(pages);
-     const handlePrev=()=>{
-       if(currentPage>0){
-         setCurrentPage(currentPage-1)
-       }
-     }
-     useEffect(() => {
-      AOS.init();
-    }, [])
-     const handleNext=()=>{
-       if(currentPage<pages.length-1){
-         setCurrentPage(currentPage+1)
-       }
-     }
-  const [courses,isFetching] = useAllClasses(currentPage,itemsPerPage);
+  //  get all classes length
+  const [currentPage, setCurrentPage] = useState(0);
+  const allApprovedClassCount = useGetAllCallsesForAllClassesPageCount();
+  // console.log( allApprovedClassCount);
+  const itemsPerPage = 10;
+  const numberOfPage = Math.ceil(allApprovedClassCount / itemsPerPage);
+  //  console.log(numberOfPage);
+  let pages = [];
+  for (let num = 0; num < numberOfPage; num++) {
+    pages.push(num);
+  }
+  console.log(pages);
+  const handlePrev = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  const handleNext = () => {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const [courses, isFetching] = useAllClasses(currentPage, itemsPerPage);
   //console.log(courses);
   console.log(courses);
-  const sortedCourses=courses.sort((a,b)=>b.Enrollment-a.Enrollment)
-  console.log('sorted array',sortedCourses);
-  if(isFetching){
-    return<LoadingSpinner></LoadingSpinner>
+  const sortedCourses = courses.sort((a, b) => b.Enrollment - a.Enrollment);
+  console.log("sorted array", sortedCourses);
+  if (isFetching) {
+    return <LoadingSpinner></LoadingSpinner>;
   }
   return (
-    <div className=" pt-20 lg:pt-28 bg-base-100">
+    <main className=" w-[95%] mx-auto md:w-[90%] lg:w-[86%] my-20 ">
       <HelmetPorvider title={"All Class"}></HelmetPorvider>
       <div className=" container  mx-auto">
-        <div>
-          <h1 className=" px-2 lg:px-0 text-3xl font-black text-center my-5 lg:my-10 md:text-5xl">
-            Knowledge Is <span className=" text-green-600">Wealth</span>
-          </h1>
-          <h3 className=" px-5 lg:px-5 text-lg lg:text-xl text-center font-bold">
-            Drive into Knowledge ocean With{" "}
-            <span className=" text-green-500">Learn Mentor Gate</span> Online
-            Courses...
-          </h3>
-        </div>
+        <Heading
+          description={"Knowledge Is Wealth"}
+          title1={"Drive into Knowledge ocean With"}
+          imp={"Learn Mentor Gate"}
+        ></Heading>
+
+        {/* sort section  */}
+        <section className=" md:mx-10 flex justify-between items-center">
+          <select name="category" className="select select-bordered  w-32">
+            <option disabled selected>
+              Category
+            </option>
+            <option value={'Design'}>Design</option>
+            <option value={'Marketing'}>Marketing</option>
+            <option value={'Programming'}>Programming</option>
+          </select>
+
+          <div className="join">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered join-item"
+            />
+            <button className="btn btn-primary join-item">Search</button>
+          </div>
+        </section>
         <div className=" my-20 grid grid-cols-1 gap-10 lg:grid-cols-2 ">
-          {sortedCourses?.map((course,index) => (
+          {sortedCourses?.map((course, index) => (
             <div
-            data-aos-duration={1000} data-aos={index%2===0?'fade-up':'fade-down'}
+              data-aos-duration={1000}
+              data-aos={index % 2 === 0 ? "fade-up" : "fade-down"}
               key={course._id}
-              className=" border bg-base-200 flex flex-col w-full p-6 space-y-6 overflow-hidden rounded-lg shadow-md  "
+              className=" border bg-base-200 bg-opacity-30 flex flex-col w-full p-6 space-y-6 overflow-hidden rounded-lg shadow-md  "
             >
-              <div className="flex border-b pb-1 border-gray-600 space-x-4">
+              <div className="flex border-b pb-1 border-gray-400 space-x-4">
                 <img
                   alt=""
                   src={course?.Instructor_Image}
@@ -79,7 +96,9 @@ const AllClasses = () => {
                   >
                     {course?.Instructor}
                   </a>
-                  <span className=" text-sm lg:text-base ">{course?.Posted_on?.split('T')[0]}</span>
+                  <span className=" text-sm lg:text-base ">
+                    {course?.Posted_on?.split("T")[0]}
+                  </span>
                 </div>
               </div>
               <div>
@@ -88,58 +107,78 @@ const AllClasses = () => {
                   alt=""
                   className=" w-full mb-4 h-60 sm:h-96  "
                 />
-                <h2 className="mb-1 text-xl lg:text-2xl font-bold">{course?.Title}</h2>
+                <h2 className="mb-1 text-xl lg:text-2xl font-bold">
+                  {course?.Title}
+                </h2>
                 <p className="text-base ">
                   {course?.["Short_description"]}
-                  <Link to={`/class/${course?._id}`} className=" text-blue-700 font-bold">see more...</Link >
+                  <Link
+                    to={`/class/${course?._id}`}
+                    className=" text-blue-700 font-bold"
+                  >
+                    see more...
+                  </Link>
                 </p>
               </div>
               <div className=" flex flex-col lg:flex-row  justify-between my-3 gap-3">
-                <h3 className=" font-bold text-base lg:text-lg">Price: <span className=" text-amber-500">{course?.Price} $</span></h3>
-                <h3 className=" font-bold text-base lg:text-lg flex items-center gap-2 ">Total Enrollments: <span className="flex gap-2 items-center text-amber-500 ">{course?.Enrollment} <FaUsers className=" text-2xl"></FaUsers></span></h3>
-
+                <h3 className=" font-bold text-base lg:text-lg">
+                  Price:{" "}
+                  <span className=" text-amber-500">{course?.Price} $</span>
+                </h3>
+                <h3 className=" font-bold text-base lg:text-lg flex items-center gap-2 ">
+                  Total Enrollments:{" "}
+                  <span className="flex gap-2 items-center text-amber-500 ">
+                    {course?.Enrollment}{" "}
+                    <FaUsers className=" text-2xl"></FaUsers>
+                  </span>
+                </h3>
               </div>
-              <h3 className=" text-base pb-3 lg:text-lg font-bold ">Duration: <span className=" text-amber-500">{course?.Duration}</span></h3>
+              <h3 className=" text-base pb-3 lg:text-lg font-bold ">
+                Duration:{" "}
+                <span className=" text-amber-500">{course?.Duration}</span>
+              </h3>
               <div className=" mb-3">
                 <Link to={`/class/${course?._id}`}>
-                <button className="text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500 ">
-                  Enroll Now!
-                </button>
+                  <button className="text-white bg-green-500 p-2 rounded-lg font-black hover:bg-white hover:text-green-700  hover:border hover:border-green-500 ">
+                    Enroll Now!
+                  </button>
                 </Link>
               </div>
             </div>
-            
           ))}
         </div>
       </div>
-      {pages.length > 0 ?  <div
-        className={` ${
-          pages.length > 10 && "overflow-scroll"
-        } flex justify-center  gap-5 bg-gray-200 w-full my-5`}
-      >  
-      <button onClick={handlePrev} className=" btn bg-gray-300"><FaLessThan></FaLessThan></button>
-     
-            {pages.map((page, index) => (
-             <div  key={page} >
-               <button
-                onClick={()=>setCurrentPage(page)}
+      {pages.length > 0 ? (
+        <div
+          className={` ${
+            pages.length > 10 && "overflow-scroll"
+          } flex justify-center  gap-5 bg-gray-200 w-full my-5`}
+        >
+          <button onClick={handlePrev} className=" btn bg-gray-300">
+            <FaLessThan></FaLessThan>
+          </button>
+
+          {pages.map((page, index) => (
+            <div key={page}>
+              <button
+                onClick={() => setCurrentPage(page)}
                 // onMouseOut={() => refetch()}
                 className={`btn text-black ${
                   currentPage === page ? "btn-warning" : "bg-gray-400"
                 }`}
-              
               >
                 {index + 1}
               </button>
-             </div>
-            ))}
-             <button 
-             onClick={handleNext}
-             className=" btn bg-gray-300"><FaGreaterThan></FaGreaterThan></button>
-      </div>: (
-          <></>
-        )}
-    </div>
+            </div>
+          ))}
+          <button onClick={handleNext} className=" btn bg-gray-300">
+            <FaGreaterThan></FaGreaterThan>
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </main>
   );
 };
 
--- template/src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
+++ template/src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -15,21 +15,21 @@ const WhyChooseUs = () => {
       <div className="">
       <h1 className=" text-xl lg:text-3xl my-5 pb-5 font-black text-center text-white">Why Choose Us </h1>
       <div className=" my-5  text-black mx-auto flex flex-col lg:flex-row gap-5">
-          <div data-aos-duration={1000} data-aos={'zoom-in'} className="stat min-w-[300px] w-full bg-green-50 border border-b-8 border-orange-300 border-r-8 ">
+          <div data-aos-duration={1000} data-aos={'zoom-in'} className="stat flex justify-center items-center flex-row-reverse min-w-[300px] w-full bg-green-50 border border-b-8 border-orange-300 border-r-8 ">
             <div className="stat-figure text-secondary">
               <img className=' w-20 h-20' src={support} alt="" />
             </div>
             
             <div className=" text-base  md:text-xl font-bold">Unlimited Support 24/7</div>
           </div>
-          <div data-aos-duration={1000} data-aos='zoom-out'   className="stat min-w-[300px] w-full bg-green-50 border border-b-8 border-purple-300 border-r-8 ">
+          <div data-aos-duration={1000} data-aos='zoom-out'   className="stat flex justify-center items-center flex-row-reverse min-w-[300px] w-full bg-green-50 border border-b-8 border-purple-300 border-r-8 ">
             <div className="stat-figure text-secondary">
               <img className=' w-20 h-20' src={budget} alt="" />
             </div>
             
             <div className=" text-base  md:text-xl font-bold">Affordable Price</div>
           </div>
-          <div  data-aos-duration={1000} data-aos='zoom-in'   className="stat min-w-[300px] w-full bg-green-50 border border-b-8 border-yellow-300 border-r-8 ">
+          <div  data-aos-duration={1000} data-aos='zoom-in'   className="stat flex justify-center items-center flex-row-reverse min-w-[300px] w-full bg-green-50 border border-b-8 border-yellow-300 border-r-8 ">
             <div className="stat-figure text-secondary">
               <img className=' w-20 h-20' src={management} alt="" />
             </div>
